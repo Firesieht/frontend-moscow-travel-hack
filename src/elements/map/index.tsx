bb0497a9a40985dac639092489edc8b8720a05e6
= <=>
2024-04-07 15:08:41 +0300

    ggg

--- src/elements/map/index.tsx
+++ src/elements/map/index.tsx
@@ -41,7 +41,7 @@ export const MyMap: React.FC<MapIE> = (props) =>{
       if (props.routeCreate != false){
           setTimeout(()=> axios.get('https://api.mapbox.com/directions/v5/mapbox/walking/'+pathString+'?alternatives=true&continue_straight=true&geometries=geojson&language=en&overview=simplified&steps=true&access_token=pk.eyJ1IjoiZmlyZXNpZWh0IiwiYSI6ImNrdW9kemYzbTB4ZGkycHAxbXN2YnIzaGMifQ.G0fl-qVbecucfOvn8OtU4Q').then(
             (data:any) => setRoute(data.data.routes[0].geometry)
-          ).catch((err)=>console.log('ERRRRRRRR')), 10000)
+          ).catch((err)=>console.log('ERRRRRRRR')), 1000)
       }
     })
       
